22e0bf7fd7092e68875bfec6024ab03dde8d8225
jjb0410 <154564136+jjb0410@users.noreply.github.com>
2024-05-02 06:19:54 -0400

    Allow etch a sketch to be sketched on

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ let grid = document.querySelector(".grid");
 
 for (i=0;i<256;i++) {
     let gridSquare = document.createElement("div");
+    gridSquare.classList.add("gridSquare");
     grid.appendChild(gridSquare);
 }
+
+let allSquares = document.querySelectorAll(".gridSquare");
+allSquares.forEach((square) => {
+    square.addEventListener("mouseover", ()=> {
+        square.classList.add("filledSquare");
+    });
+});
